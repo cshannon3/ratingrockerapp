3ccc849123210255745b74f63542221882402dad
cshannon3 <33521708+cshannon3@users.noreply.github.com>
2017-12-24 14:12:23 -0500

    Update Vibeonedata.js

--- Vibeonedata.js
+++ Vibeonedata.js
@@ -8,33 +8,46 @@ public class Vibeonedata {
     private int _ratingval;
     private int _ratecount;
     private int _freshvalue;
-    private int _songid;
+    private String _songid;
+    private String _songname;
 
 
     public Vibeonedata() {
 
     }
 
-    public Vibeonedata(int _ratingval, int _songid, int _ratecount, int _freshvalue) {
-        this._songid = _songid;
-        this._ratingval = _ratingval;
-        this._ratecount = _ratecount;
-        this._freshvalue = _freshvalue;
-    }
+    public Vibeonedata(int ratingval, String songid, String songname, int ratecount, int freshvalue ) {
 
-    public Vibeonedata(int _id, int _songid, int _ratingval, int _ratecount, int _freshvalue) {
+        this._ratingval = ratingval;
+        this._songid = songid;
+        this._songname = songname;
+        this._ratecount = ratecount;
+        this._freshvalue = freshvalue;
+
+    }
+/*
+    public Vibeonedata(int _id, String _songid, String _songname, int _ratingval, int _ratecount, int _freshvalue) {
         this._id = _id;
         this._songid = _songid;
         this._ratingval = _ratingval;
         this._ratecount = _ratecount;
         this._freshvalue = _freshvalue;
+        this._songname = _songname;
+    }
+*/
+    public String get_songname() {
+        return _songname;
     }
 
-    public int get_songid() {
+    public void set_songname(String _songname) {
+        this._songname = _songname;
+    }
+
+    public String get_songid() {
         return _songid;
     }
 
-    public void set_songid(int _songid) {
+    public void set_songid(String _songid) {
         this._songid = _songid;
     }
 
@@ -43,7 +56,7 @@ public class Vibeonedata {
     }
 
     public void set_id(int _id) {
-        this._id = _id;
+       this._id = _id;
     }
 
     public int get_ratingval() {
